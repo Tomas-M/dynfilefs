5d042a42460d583172ea1bb98597725926371b90
TomasM <tomas@slax.org>
2017-11-11 16:14:15 -0500

    added buildroot scripts

---
+++ buildroot/httpfs.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# dynfilefs
+#
+################################################################################
+
+DYNFILEFS_VERSION = master
+DYNFILEFS_SOURCE = dynfilefs-v1.2.tar.gz
+DYNFILEFS_SITE = https://github.com/Tomas-M/dynfilefs/archive
+DYNFILEFS_LICENSE = GPLv2+, LGPLv2+
+DYNFILEFS_DEPENDENCIES = host-pkgconf libfuse
+DYNFILEFS_MAKE_OPTS = LDFLAGS=-static
+
+$(eval $(autotools-package))

8c4b47a3187f79b082ddd2e3fe08bc52b71cf5db
Tomas Matejicek <tomas@slax.org>
2023-01-01 10:22:08 +0100

    update mk file for bootstrap

--- buildroot/httpfs.mk
+++ buildroot/httpfs.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 DYNFILEFS_VERSION = master
-DYNFILEFS_SOURCE = dynfilefs-v1.2.tar.gz
+DYNFILEFS_SOURCE = dynfilefs-v3.0.tar.gz
 DYNFILEFS_SITE = https://github.com/Tomas-M/dynfilefs/archive
 DYNFILEFS_LICENSE = GPLv2+, LGPLv2+
 DYNFILEFS_DEPENDENCIES = host-pkgconf libfuse
